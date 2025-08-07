ffe40642d422ea6ec1d0f4b324ae8cd7bc190d9d
FaiziCodeSpace <faizanwebdev1@gmail.com>
2025-08-07 16:53:31 +0500

    Add route to filter students by major and age using custom query helpers

--- models/Student.js
+++ models/Student.js
@@ -1,43 +1,54 @@
 import mongoose from "mongoose";
 
 const studentSchema = new mongoose.Schema({
-    name: {
-        type:String,
-        required: [true, "Name is required"],
-        minlength: [3, "Name should be more than 3 characters"],
-        trim: true,
-    },
-    age: {
-        type:Number,
-        required:[true, "Age is required"],
-        min: [5, "Too Young"],
-        max: [100, "Too Old"],
-    },
-    major: {
-        type: String,
-        required: [true, "Major is required"],
-        default: true
-    },
-    enrolledAt: {
-        type: Date,
-        default: Date.now
-    },
-      teacher: {
-    type: mongoose.Schema.Types.ObjectId,  
-    ref: "Teacher",                        
-  }
-
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    minlength: [3, "Name should be more than 3 characters"],
+    trim: true,
+  },
+  age: {
+    type: Number,
+    required: [true, "Age is required"],
+    min: [5, "Too Young"],
+    max: [100, "Too Old"],
+  },
+  major: {
+    type: String,
+    required: [true, "Major is required"],
+    default: true,
+  },
+  enrolledAt: {
+    type: Date,
+    default: Date.now,
+  },
+  teacher: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Teacher",
+  },
+  department: String,
 });
 
 // middleware
-studentSchema.pre('save', function(next){
-    if(this.name){
-        this.name = this.name
-        .split(" ")
-        .map((word)=> word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-        .join(" ");
-    }
-    next();
-})
+studentSchema.pre("save", function (next) {
+  if (this.name) {
+    this.name = this.name
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(" ");
+  }
+  next();
+});
 
-export default mongoose.model('Student', studentSchema);
+// Query Helper
+studentSchema.query.byMajor = function (major) {
+  return this.where({ major: new RegExp(`^${major}$`, 'i') }); // case-insensitive match
+};
+
+studentSchema.query.byAge = function (age) {
+  return this.where({ age: age });
+};
+
+const Student = mongoose.model("Student", studentSchema);
+
+export default Student;
